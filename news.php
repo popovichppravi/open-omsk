561cee801689c805bb5e6dce87d64ab6d91113e1
popovichppravi <popovichppravi@gmail.com>
2016-10-13 07:03:20 +0300

    страница news

---
+++ news.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- CSS files-->
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/media.css" rel="stylesheet">
+    <link href="css/fonts.css" rel="stylesheet">
+    <link href="css/colors.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  </head>
+  <body>
+  <!--HEADER-->
+    <header>
+      <div class="top_wrap">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <!--TOP LOGO-->
+              <div class="top_logo">
+                <a href="#">
+                  <img src="images/gerb.png" alt="Открытые данные администрации города Омска">
+                  <div class="logo_txt">
+                    <p class="top_logo_txt">Открытые данные</p>
+                    <p class="bottom_logo_txt">Администрации города Омска</p>
+                  </div>
+                </a>
+              </div>
+            <!--//TOP LOGO-->
+              <a href="/" class="hidden-lg hidden-md hidden-xs pull-right">Портал «Омск.РФ»</a>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-right">
+              <a href="/" class="hidden-sm">Портал «Омск.РФ»</a>
+            <!--TOP SEARCH-->  
+              <form action="#" id="top-search">
+                <input type="text" class="form-control" placeholder="Поиск..."/>
+                <button><i class="fa fa-search" aria-hidden="true"></i></button>
+              </form>
+            <!--//TOP SEARCH--> 
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--MOBILE MENU--> 
+      <div class="mobile_menu hidden-lg hidden-md hidden-sm">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <a href="#" id="toggle_menu"><p>Меню</p><span></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--//MOBILE MENU-->    
+
+      <!--TOP MENU-->
+      <nav class="top_menu">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <ul>
+                <li class="active"><a href="#">Наборы данных</a></li>
+                <li><a href="#">О проекте</a></li>
+                <li><a href="#">Новости</a></li>
+                <li><a href="#">Вопросы и ответы</a></li>
+                <li><a href="#">Контакты</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>       
+      </nav>
+      <!--//TOP MENU-->
+    </header>
+  <!--//HEADER-->
+
+  <!--CONTENT-->
+<article class="content">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12">
+        <h1>Новости</h1>
+
+        <!--NEWS-->
+        <div class="news">
+          <div class="item">
+            <div class="row">
+             <div class="col-lg-10 col-xs-9">
+              <a href="#" class="title_item">30 сентября в Омске официально завершился масштабный ремонт дорог</a>
+             </div>
+
+          <div class="col-lg-2 col-xs-3">
+            <div class="date left_i">
+              <i class="fa fa-calendar-o" aria-hidden="true"></i><span>10 мая 2016 г.</span>
+            </div>
+          </div>
+        </div>
+
+          <p>Напомним, что в 2016 году в городе удалось существенно увеличить объёмы ремонта дорог общего пользования. Благодаря привлечению дополнительных финансовых ресурсов из областного и федерального бюджетов, в текущем году выполнен ремонт на 57 магистралях.</p>
+          </div>
+
+               <div class="item">
+            <div class="row">
+             <div class="col-lg-10 col-xs-9">
+              <a href="#" class="title_item">Победителем туристического слета стала команда Колосовского детского дома</a>
+             </div>
+
+          <div class="col-lg-2 col-xs-3">
+            <div class="date left_i">
+              <i class="fa fa-calendar-o" aria-hidden="true"></i><span>10 июня 2016 г.</span>
+            </div>
+          </div>
+        </div>
+
+          <p>На территории санатория «Колос» прошел VII областной туристический слет для воспитанников детских домов, школ-интернатов и детей-сирот города Омска и Омской области.</p>
+          </div>
+
+
+               <div class="item">
+            <div class="row">
+             <div class="col-lg-10 col-xs-9">
+              <a href="#" class="title_item">Городской музей «Искусство Омска» представляет выставку «Сокровенное»</a>
+             </div>
+
+          <div class="col-lg-2 col-xs-3">
+            <div class="date left_i">
+              <i class="fa fa-calendar-o" aria-hidden="true"></i><span>3 октября 2016 г.</span>
+            </div>
+          </div>
+        </div>
+
+          <p>3 октября в 16:00 в выставочном зале музея «Искусство Омска» открывается выставка работ преподавателей изобразительного искусства — «Сокровенное».</p>
+          </div>
+
+
+               <div class="item">
+            <div class="row">
+             <div class="col-lg-10 col-xs-9">
+              <a href="#" class="title_item">30 сентября в Омске официально завершился масштабный ремонт дорог</a>
+             </div>
+
+          <div class="col-lg-2 col-xs-3">
+            <div class="date left_i">
+              <i class="fa fa-calendar-o" aria-hidden="true"></i><span>10 мая 2016 г.</span>
+            </div>
+          </div>
+        </div>
+
+          <p>Напомним, что в 2016 году в городе удалось существенно увеличить объёмы ремонта дорог общего пользования. Благодаря привлечению дополнительных финансовых ресурсов из областного и федерального бюджетов, в текущем году выполнен ремонт на 57 магистралях.</p>
+          </div>
+
+
+               <div class="item">
+            <div class="row">
+             <div class="col-lg-10 col-xs-9">
+              <a href="#" class="title_item">Омич Семен Семенов завоевал две медали Всероссийского турнира</a>
+             </div>
+
+          <div class="col-lg-2 col-xs-3">
+            <div class="date left_i">
+              <i class="fa fa-calendar-o" aria-hidden="true"></i><span>10 мая 2016 г.</span>
+            </div>
+          </div>
+        </div>
+
+          <p>Напомним, что в 2016 году в городе удалось существенно увеличить объёмы ремонта дорог общего пользования. Благодаря привлечению дополнительных финансовых ресурсов из областного и федерального бюджетов, в текущем году выполнен ремонт на 57 магистралях.</p>
+          </div>
+
+
+               <div class="item">
+            <div class="row">
+             <div class="col-lg-10 col-xs-9">
+              <a href="#" class="title_item">30 сентября в Омске официально завершился масштабный ремонт дорог</a>
+             </div>
+
+          <div class="col-lg-2 col-xs-3">
+            <div class="date left_i">
+              <i class="fa fa-calendar-o" aria-hidden="true"></i><span>10 мая 2016 г.</span>
+            </div>
+          </div>
+        </div>
+
+          <p>Напомним, что в 2016 году в городе удалось существенно увеличить объёмы ремонта дорог общего пользования. Благодаря привлечению дополнительных финансовых ресурсов из областного и федерального бюджетов, в текущем году выполнен ремонт на 57 магистралях.</p>
+          </div>
+
+        </div>
+
+        <div class="text-right">
+<ul class="pagination">
+  <li class="disabled"><a href="#">&laquo;</a></li>
+  <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+    <li><a href="#">2</a></li>
+  <li><a href="#">3</a></li>
+  <li><a href="#">4</a></li>
+  <li><a href="#">5</a></li>
+   <li><a href="#">&raquo;</a></li>
+</ul>
+  </div>
+        <!--//NEWS-->
+  
+      </div>
+    </div>
+  </div>
+  
+</article>
+  <!--//CONTENT-->
+
+  <!--FOOTER-->
+  <footer>
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12">
+                  <p>© 2016 Администрация города Омска.</p>
+                  <p>При использовании открытых данных ссылка на <a href="http://opendata.admomsk.ru" target="_blank">opendata.admomsk.ru</a> обязательна.</p>
+                  <p>Поддержка: Казенное учреждение города Омска «Управление информационно-коммуникационных технологий».</p>
+              </div>
+          </div>
+      </div>
+  </footer>
+  <!--//FOOTER-->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/common.js"></script>
+  </body>
+</html>
