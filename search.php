3655bca969748c3ed7e0c0091d6460ead70e039a
popovichppravi <popovichppravi@gmail.com>
2016-10-13 05:39:01 +0300

    паспорт набора данных

---
+++ search.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Комментарии к набору данных</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- CSS files-->
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/media.css" rel="stylesheet">
+    <link href="css/fonts.css" rel="stylesheet">
+    <link href="css/colors.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  </head>
+  <body>
+  <!--HEADER-->
+    <header>
+      <div class="top_wrap">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <!--TOP LOGO-->
+              <div class="top_logo">
+                <a href="#">
+                  <img src="images/gerb.png" alt="Открытые данные администрации города Омска">
+                  <div class="logo_txt">
+                    <p class="top_logo_txt">Открытые данные</p>
+                    <p class="bottom_logo_txt">Администрации города Омска</p>
+                  </div>
+                </a>
+              </div>
+            <!--//TOP LOGO-->
+              <a href="/" class="hidden-lg hidden-md hidden-xs pull-right">Портал «Омск.РФ»</a>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-right">
+              <a href="/" class="hidden-sm">Портал «Омск.РФ»</a>
+            <!--TOP SEARCH-->  
+              <form action="#" id="top-search">
+                <input type="text" class="form-control" placeholder="Поиск..."/>
+                <button><i class="fa fa-search" aria-hidden="true"></i></button>
+              </form>
+            <!--//TOP SEARCH--> 
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--MOBILE MENU--> 
+      <div class="mobile_menu hidden-lg hidden-md hidden-sm">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <a href="#" id="toggle_menu"><p>Меню</p><span></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--//MOBILE MENU-->    
+
+      <!--TOP MENU-->
+      <nav class="top_menu">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <ul>
+                <li class="active"><a href="#">Наборы данных</a></li>
+                <li><a href="#">О проекте</a></li>
+                <li><a href="#">Новости</a></li>
+                <li><a href="#">Вопросы и ответы</a></li>
+                <li><a href="#">Контакты</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>       
+      </nav>
+      <!--//TOP MENU-->
+    </header>
+  <!--//HEADER-->
+
+  <!--CONTENT-->
+<article class="content">
+  <div class="container">
+    <div class="title_block">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <h1>Летние детские площадки с вожатыми</h1>
+        </div>
+      </div>
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+              <div class="info_icons_block">
+                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i><span>78</span>
+                    <i class="fa fa-eye" aria-hidden="true"></i><span>345</span>
+                    <i class="fa fa-download" aria-hidden="true"></i><span>24</span>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right">
+              <a href="#" class="left_i"><i class="fa fa-long-arrow-left" aria-hidden="true"></i>вернуться к набору</a>
+            </div>
+          </div>
+    </div>
+  </div>
+
+  <!--BG BLOCK-->
+  <div class="bg_block">
+    <div class="container">
+      <div class="table_cell">
+        <div class="cell_block">
+        <h2>Паспорт набора данных</h2>        
+        </div>
+        <div class="text-right cell_block ">
+          <p>Обновлено: 16 мая 2016 г.<br />Опубликовано: 15 мая 2014 г.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--//BG BLOCK-->
+  <br />
+  
+
+<div class="container">
+  <div class="row">
+    <!--CONTENT BLOCK-->
+    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 page" role="main">
+
+<h3>Описание набора</h3>
+<p>В наборе дано местоположение всех детских площадок, где летом 2016 года во второй половине дня с детьми бесплатно занимаются специально подготовленные вожатые педагогических отрядов и волонтеры в рамках программы летней занятости.</p>
+<h3>Ключевые слова</h3>
+<p>Лето; дети; вожатые; педагогический отряд; педотряд; подвижные игры; эстафеты; досуг; развлечения; бесплатно; вторая половина дня; летняя занятость; подростки.</p>
+<h3>Структура набора и комментарии</h3>
+<table class="tbl">
+  <tr>
+    <td><b>Местоположение</b></td>
+    <td>Адреса близлежащих домов</td>
+  </tr>
+  <tr>
+    <td><b>Координаты</b></td>
+    <td>Широта и долгота</td>
+  </tr>
+  <tr>
+    <td><b>Малые архитектурные формы</b></td>
+    <td>Что установлено на площадке</td>
+  </tr>
+  <tr>
+    <td><b>Ответственные за работу</b></td>
+    <td>Имена и мобильные телефоны<br />сотрудников Городского центра социальных программ</td>
+  </tr>
+  <tr>
+    <td><b>Время работы вожатых</b></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>Округ</b></td>
+    <td></td>
+  </tr>
+</table>
+
+<h3>Содержание последнего изменения</h3>
+<p>Важно: изменен порядок следования координат! Также обновлены площадки по Кировскому и Ленинскому округам. Актуализированы телефоны ответственных за работу площадок по Центральному и Октябрьскому округам.</p>
+<h3>Постоянный адрес набора</h3>
+<p><a href="#">http://opendata.admomsk.ru/5503093917-summer</a></p>     
+      </div>
+    <!--//CONTENT BLOCK-->
+      
+    <!--RIGHT SIDEBAR-->
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+      <div id="sidebar_right" > 
+        <div class="sidebar_block">
+        <div class="title">Рубрика</div>
+          <p><a href="#">Спорт и молодежная политика</a></p>
+        </div>
+
+        <div class="sidebar_block">
+        <div class="title">Поставщик набора</div>
+          <p><a href="#">Департамент по делам молодежи, физической культуре и спорту</a></p>
+        </div>
+
+         <div class="sidebar_block">
+        <div class="title">Ответственное лицо</div>
+          <p>Мария Петровна Иванова, главный специалист</p>
+          <div class="contacts_sidebar">
+          <p><i class="fa fa-phone" aria-hidden="true"></i><a href="#">8 (381-2) 12-34-56</a></p>
+          <p><i class="fa fa-envelope" aria-hidden="true"></i><a href="#">ivanova@admomsk.ru</a></p>               
+          </div>
+        </div>
+
+         <div class="sidebar_block">
+          <div class="title">Частота обновления</div>
+          <p>Не реже одного раза в год</p>
+        </div> 
+
+         <div class="sidebar_block">
+        <a href="#">Устаревшие версии набора (3)</a> 
+      </div> 
+
+       <div class="sidebar_block">
+          <div class="title">Формат набора</div>
+          <p>CSV, XLS</p>
+        </div> 
+
+        <div class="sidebar_block">
+          <div class="title">Индентификатор набора</div>
+          <p>5503093917-summer</p>
+        </div> 
+
+
+        <div class="sidebar_block">
+          <div class="bg_block">
+            <div class="download_top_block">
+              <ul>
+                  <li>Скачать:</li>
+                  <li><a href="#">csv</a><span>(2Кб)</span></li>
+              </ul>
+            </div>
+          </div>
+        </div> 
+
+
+
+           
+      </div>
+    </div>
+    <!--//RIGHT SIDEBAR--> 
+
+    </div>
+  </div>
+</article>
+  <!--//CONTENT-->
+
+  <!--FOOTER-->
+  <footer>
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12">
+                  <p>© 2016 Администрация города Омска.</p>
+                  <p>При использовании открытых данных ссылка на <a href="http://opendata.admomsk.ru" target="_blank">opendata.admomsk.ru</a> обязательна.</p>
+                  <p>Поддержка: Казенное учреждение города Омска «Управление информационно-коммуникационных технологий».</p>
+              </div>
+          </div>
+      </div>
+  </footer>
+  <!--//FOOTER-->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/common.js"></script>
+  </body>
+</html>
