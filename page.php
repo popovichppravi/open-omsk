f0529d5163feca7b17392e39498c7d666893a418
popovichppravi <popovichppravi@gmail.com>
2016-10-13 07:33:33 +0300

    стандартная страница

---
+++ page.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- CSS files-->
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/media.css" rel="stylesheet">
+    <link href="css/fonts.css" rel="stylesheet">
+    <link href="css/colors.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  </head>
+  <body>
+  <!--HEADER-->
+    <header>
+      <div class="top_wrap">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <!--TOP LOGO-->
+              <div class="top_logo">
+                <a href="#">
+                  <img src="images/gerb.png" alt="Открытые данные администрации города Омска">
+                  <div class="logo_txt">
+                    <p class="top_logo_txt">Открытые данные</p>
+                    <p class="bottom_logo_txt">Администрации города Омска</p>
+                  </div>
+                </a>
+              </div>
+            <!--//TOP LOGO-->
+              <a href="/" class="hidden-lg hidden-md hidden-xs pull-right">Портал «Омск.РФ»</a>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-right">
+              <a href="/" class="hidden-sm">Портал «Омск.РФ»</a>
+            <!--TOP SEARCH-->  
+              <form action="#" id="top-search">
+                <input type="text" class="form-control" placeholder="Поиск..."/>
+                <button><i class="fa fa-search" aria-hidden="true"></i></button>
+              </form>
+            <!--//TOP SEARCH--> 
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--MOBILE MENU--> 
+      <div class="mobile_menu hidden-lg hidden-md hidden-sm">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <a href="#" id="toggle_menu"><p>Меню</p><span></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--//MOBILE MENU-->    
+
+      <!--TOP MENU-->
+      <nav class="top_menu">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <ul>
+                <li class="active"><a href="#">Наборы данных</a></li>
+                <li><a href="#">О проекте</a></li>
+                <li><a href="#">Новости</a></li>
+                <li><a href="#">Вопросы и ответы</a></li>
+                <li><a href="#">Контакты</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>       
+      </nav>
+      <!--//TOP MENU-->
+    </header>
+  <!--//HEADER-->
+
+  <!--CONTENT-->
+<article class="content">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12">
+        <h1>h1 Заголовок</h1>
+        <h2>h2 Заголовок</h1>
+        <h3>h3 Заголовок</h1>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab laudantium sint distinctio itaque ratione vitae ad, molestias rem laborum. Iste architecto rem minus sed repellat similique cumque ipsum ad. Nemo atque animi beatae nostrum, odit voluptas veniam est. Nisi fugit aut, corrupti nihil? Nostrum eos in voluptas fugit odio, natus quas voluptatem voluptatum suscipit tenetur dolorum, dolorem cum, expedita ut eaque hic odit consectetur error alias! Aliquid perferendis iste ea soluta, eius ut. Aliquid odit esse dolorem quo ex!</p>
+         <p>Quae libero veritatis culpa, itaque reprehenderit, ipsum sapiente placeat eos. Dolore blanditiis, eaque aperiam deserunt corporis fuga odio consequuntur. Vitae harum, assumenda sunt, autem quis eius iste excepturi vero aut minus, nesciunt? Iusto, voluptate esse? Beatae sapiente molestiae iure dolorem, pariatur sed, ex exercitationem quas odit magni delectus veritatis quae. Modi obcaecati veniam sequi expedita in repudiandae earum doloribus nulla quod, est nam quisquam quae voluptatum ipsa et.</p>
+<br />
+         <p>Маркированный список:</p>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ul>
+<br />
+<p>Нумерованный список:</p>
+        <ol>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ol>
+<br />
+        <p>
+          <img src="images/img1.jpg" alt="г. Омск">
+          <div class="img_info">
+          <p>город Омск</p>
+          <i><small>автор: Иванов Иван Иванович</small></i>
+        </div>
+        </p>
+
+        <br />
+<blockquote>
+  <p>A cumque qui quas perferendis maiores, repudiandae voluptate laborum ad delectus voluptatibus aspernatur natus rem possimus dolor corrupti. Maxime quaerat a reprehenderit voluptas nam unde eligendi dicta eius officiis ut quam, dolores, soluta dolore vero praesentium. </p>
+</blockquote>
+
+<br />
+<h3>Таблица</h3>
+<table class="tbl">
+  <tbody><tr>
+    <td><b>Местоположение</b></td>
+    <td>Адреса близлежащих домов</td>
+  </tr>
+  <tr>
+    <td><b>Координаты</b></td>
+    <td>Широта и долгота</td>
+  </tr>
+  <tr>
+    <td><b>Малые архитектурные формы</b></td>
+    <td>Что установлено на площадке</td>
+  </tr>
+  <tr>
+    <td><b>Ответственные за работу</b></td>
+    <td>Имена и мобильные телефоны<br>сотрудников Городского центра социальных программ</td>
+  </tr>
+  <tr>
+    <td><b>Время работы вожатых</b></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>Округ</b></td>
+    <td></td>
+  </tr>
+</tbody></table>
+
+<br />
+
+<h3>Пример формы</h3>
+
+<!--COMMENTS FORM-->
+<form class="form-horizontal" role="form">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-4 control-label">Имя *</label>
+    <div class="col-sm-8">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Введите имя">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-4 control-label">Комментарий *</label>
+    <div class="col-sm-8">
+     <textarea class="form-control" rows="6" placeholder="Введите текст сообщения"></textarea>
+    </div>
+  </div>
+  <div class="form-group form_btns">
+    <div class="col-sm-12 text-right left_i">
+      <button type="submit" class="btn btn-default"><i class="fa fa-times" aria-hidden="true"></i><span>Отменить</span></button><button type="submit" class="btn btn-default"><i class="fa fa-check" aria-hidden="true"></i><span>Отправить</span></button>
+    </div>
+  </div>
+</form>
+<!--//COMMENTS FORM-->
+
+
+
+        
+  
+      </div>
+    </div>
+  </div>
+  
+</article>
+  <!--//CONTENT-->
+
+  <!--FOOTER-->
+  <footer>
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12">
+                  <p>© 2016 Администрация города Омска.</p>
+                  <p>При использовании открытых данных ссылка на <a href="http://opendata.admomsk.ru" target="_blank">opendata.admomsk.ru</a> обязательна.</p>
+                  <p>Поддержка: Казенное учреждение города Омска «Управление информационно-коммуникационных технологий».</p>
+              </div>
+          </div>
+      </div>
+  </footer>
+  <!--//FOOTER-->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/common.js"></script>
+  </body>
+</html>
