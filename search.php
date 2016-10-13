16c69dd590f94e58be9a49cc9cc4b8e27840c801
popovichppravi <popovichppravi@gmail.com>
2016-10-13 06:25:00 +0300

    страница поиска

--- search.php
+++ search.php
@@ -48,12 +48,6 @@
 
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-right">
               <a href="/" class="hidden-sm">Портал «Омск.РФ»</a>
-            <!--TOP SEARCH-->  
-              <form action="#" id="top-search">
-                <input type="text" class="form-control" placeholder="Поиск..."/>
-                <button><i class="fa fa-search" aria-hidden="true"></i></button>
-              </form>
-            <!--//TOP SEARCH--> 
             </div>
           </div>
         </div>
@@ -97,147 +91,143 @@
     <div class="title_block">
       <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <h1>Летние детские площадки с вожатыми</h1>
-        </div>
-      </div>
+          <h1>Поиск по наборам открытых данных</h1>
+
+<!--CONTEN SEARCH-->  
           <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-              <div class="info_icons_block">
-                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i><span>78</span>
-                    <i class="fa fa-eye" aria-hidden="true"></i><span>345</span>
-                    <i class="fa fa-download" aria-hidden="true"></i><span>24</span>
+            <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+              <form action="#" id="top-search">
+                <input type="text" class="form-control" placeholder="Поиск..." value="педагогических отрядов" />
+                <button><i class="fa fa-search" aria-hidden="true"></i></button>
+              </form>
+            </div>
+          </div>
+<!--//CONTENT SEARCH--> 
+
+<div>
+
+<!--SEARCH LIST-->
+<div class="search_list">
+  <div class="item">
+   <a href="#" class="title_item">Летние детские площадки с вожатыми</a> 
+          <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-7">
+             <div class="info_icons_block">
+                <i class="fa fa-pencil-square-o" aria-hidden="true"></i><span>78</span>
+                <i class="fa fa-eye" aria-hidden="true"></i><span>345</span>
+                <i class="fa fa-download" aria-hidden="true"></i><span>24</span>
+               </div>
+               <div class="middle_info_item text-justify">
+               <p>...где летом 2016 года во второй половине дня с детьми бесплатно занимаются специально подготовленные вожатые <b class="orng">педагогических отрядов</b> и волонтеры в рамках программы...</p>
+            </div>
+             
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-5">
+              <div class="rating text-right">
+                <ul>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star-half-o" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+                </ul>
               </div>
+              <div class="text-right dates">
+              <p>Обновлено: 16 мая 2016 г.</p>
+              <p>Опубликовано: 15 мая 2014 г.</p>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right">
-              <a href="#" class="left_i"><i class="fa fa-long-arrow-left" aria-hidden="true"></i>вернуться к набору</a>
             </div>
           </div>
-    </div>
-  </div>
-
-  <!--BG BLOCK-->
-  <div class="bg_block">
-    <div class="container">
-      <div class="table_cell">
-        <div class="cell_block">
-        <h2>Паспорт набора данных</h2>        
         </div>
-        <div class="text-right cell_block ">
-          <p>Обновлено: 16 мая 2016 г.<br />Опубликовано: 15 мая 2014 г.</p>
+
+  <div class="item">
+   <a href="#" class="title_item">Клубы для детей и молодежи</a> 
+          <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-7">
+             <div class="info_icons_block">
+                <i class="fa fa-pencil-square-o" aria-hidden="true"></i><span>78</span>
+                <i class="fa fa-eye" aria-hidden="true"></i><span>345</span>
+                <i class="fa fa-download" aria-hidden="true"></i><span>24</span>
+               </div>
+               <div class="middle_info_item text-justify">
+                 <p>...Клуб «Каравелла-2» • Проспект Мира, 173/2 • Фитнес, эстрадно-спортивные танцы, <b class="orng">педагогический отряд</b> Советского округа, спортивно-бальные танцы, любительское объединение...</p>
+            </div>
+             
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-5">
+              <div class="rating text-right">
+                <ul>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star-half-o" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+                </ul>
+              </div>
+              <div class="text-right dates">
+              <p>Обновлено: 16 мая 2016 г.</p>
+              <p>Опубликовано: 15 мая 2014 г.</p>
+            </div>
+            </div>
+          </div>
+        </div>
+
+          <div class="item">
+   <a href="#" class="title_item">Мероприятия Дня города — 2015</a> 
+          <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-7">
+             <div class="info_icons_block">
+                <i class="fa fa-pencil-square-o" aria-hidden="true"></i><span>78</span>
+                <i class="fa fa-eye" aria-hidden="true"></i><span>345</span>
+                <i class="fa fa-download" aria-hidden="true"></i><span>24</span>
+               </div>
+               <div class="middle_info_item text-justify">
+                 <p>...игра «Зарница», поселок Черемушки. Проводится для молодежи Ленинского округа, в том числе ребят из <b class="orng">педагогических отрядов</b>, работающей молодежи...</p>
+            </div>
+             
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-5">
+              <div class="rating text-right">
+                <ul>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star-half-o" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+                </ul>
+              </div>
+              <div class="text-right dates">
+              <p>Обновлено: 16 мая 2016 г.</p>
+              <p>Опубликовано: 15 мая 2014 г.</p>
+            </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="text-right">
+<ul class="pagination">
+  <li class="disabled"><a href="#">&laquo;</a></li>
+  <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+    <li><a href="#">2</a></li>
+  <li><a href="#">3</a></li>
+  <li><a href="#">4</a></li>
+  <li><a href="#">5</a></li>
+   <li><a href="#">&raquo;</a></li>
+</ul>
+  </div>
+</div>
+<!--//SEARCH LIST-->
+
+</div>
+
         </div>
       </div>
     </div>
   </div>
-  <!--//BG BLOCK-->
   <br />
-  
-
-<div class="container">
-  <div class="row">
-    <!--CONTENT BLOCK-->
-    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 page" role="main">
-
-<h3>Описание набора</h3>
-<p>В наборе дано местоположение всех детских площадок, где летом 2016 года во второй половине дня с детьми бесплатно занимаются специально подготовленные вожатые педагогических отрядов и волонтеры в рамках программы летней занятости.</p>
-<h3>Ключевые слова</h3>
-<p>Лето; дети; вожатые; педагогический отряд; педотряд; подвижные игры; эстафеты; досуг; развлечения; бесплатно; вторая половина дня; летняя занятость; подростки.</p>
-<h3>Структура набора и комментарии</h3>
-<table class="tbl">
-  <tr>
-    <td><b>Местоположение</b></td>
-    <td>Адреса близлежащих домов</td>
-  </tr>
-  <tr>
-    <td><b>Координаты</b></td>
-    <td>Широта и долгота</td>
-  </tr>
-  <tr>
-    <td><b>Малые архитектурные формы</b></td>
-    <td>Что установлено на площадке</td>
-  </tr>
-  <tr>
-    <td><b>Ответственные за работу</b></td>
-    <td>Имена и мобильные телефоны<br />сотрудников Городского центра социальных программ</td>
-  </tr>
-  <tr>
-    <td><b>Время работы вожатых</b></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td><b>Округ</b></td>
-    <td></td>
-  </tr>
-</table>
-
-<h3>Содержание последнего изменения</h3>
-<p>Важно: изменен порядок следования координат! Также обновлены площадки по Кировскому и Ленинскому округам. Актуализированы телефоны ответственных за работу площадок по Центральному и Октябрьскому округам.</p>
-<h3>Постоянный адрес набора</h3>
-<p><a href="#">http://opendata.admomsk.ru/5503093917-summer</a></p>     
-      </div>
-    <!--//CONTENT BLOCK-->
-      
-    <!--RIGHT SIDEBAR-->
-    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-      <div id="sidebar_right" > 
-        <div class="sidebar_block">
-        <div class="title">Рубрика</div>
-          <p><a href="#">Спорт и молодежная политика</a></p>
-        </div>
-
-        <div class="sidebar_block">
-        <div class="title">Поставщик набора</div>
-          <p><a href="#">Департамент по делам молодежи, физической культуре и спорту</a></p>
-        </div>
-
-         <div class="sidebar_block">
-        <div class="title">Ответственное лицо</div>
-          <p>Мария Петровна Иванова, главный специалист</p>
-          <div class="contacts_sidebar">
-          <p><i class="fa fa-phone" aria-hidden="true"></i><a href="#">8 (381-2) 12-34-56</a></p>
-          <p><i class="fa fa-envelope" aria-hidden="true"></i><a href="#">ivanova@admomsk.ru</a></p>               
-          </div>
-        </div>
-
-         <div class="sidebar_block">
-          <div class="title">Частота обновления</div>
-          <p>Не реже одного раза в год</p>
-        </div> 
-
-         <div class="sidebar_block">
-        <a href="#">Устаревшие версии набора (3)</a> 
-      </div> 
-
-       <div class="sidebar_block">
-          <div class="title">Формат набора</div>
-          <p>CSV, XLS</p>
-        </div> 
-
-        <div class="sidebar_block">
-          <div class="title">Индентификатор набора</div>
-          <p>5503093917-summer</p>
-        </div> 
-
-
-        <div class="sidebar_block">
-          <div class="bg_block">
-            <div class="download_top_block">
-              <ul>
-                  <li>Скачать:</li>
-                  <li><a href="#">csv</a><span>(2Кб)</span></li>
-              </ul>
-            </div>
-          </div>
-        </div> 
-
-
-
-           
-      </div>
-    </div>
-    <!--//RIGHT SIDEBAR--> 
-
-    </div>
-  </div>
 </article>
   <!--//CONTENT-->
 
